84c1754338d3aeef0d65351c6219fb9936d43548
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-01-30 18:04:35 -0300

    Adiciona repositório do typeORM

--- src/database/migrations/1611861134662-CreateAppointments.ts
+++ src/database/migrations/1611861134662-CreateAppointments.ts
@@ -13,6 +13,7 @@ export default class CreateAppointments1611861134662
             type: 'varchar',
             isPrimary: true,
             generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
             name: 'provider',
