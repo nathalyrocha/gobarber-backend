8dbb61cdea8e0c81432c928aecc5ad6688c3c325
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-02-05 16:43:34 -0300

    Adiciona service de email de recuperação de senha

---
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -0,0 +1,41 @@
+import { inject, injectable } from 'tsyringe';
+
+import IMailProvider from 'shared/container/providers/MailProvider/models/IMailProvider';
+import AppError from '../../../shared/errors/AppError';
+import IUsersRepository from '../repositories/IUsersRepository';
+import IUserTokenRepository from '../repositories/IUserTokensRepository';
+
+interface IRequest {
+  email: string;
+}
+
+@injectable()
+class SendForgotPasswordEmailService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('MailProvider')
+    private mailProvider: IMailProvider,
+
+    @inject('UserTokensRepository')
+    private userTokensRepository: IUserTokenRepository,
+  ) {}
+
+  public async execute({ email }: IRequest): Promise<void> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('User does not exists.');
+    }
+
+    const { token } = await this.userTokensRepository.generate(user.id);
+
+    await this.mailProvider.sendMail(
+      email,
+      `Pedido de recuperação de senha recebido: ${token}`,
+    );
+  }
+}
+
+export default SendForgotPasswordEmailService;
