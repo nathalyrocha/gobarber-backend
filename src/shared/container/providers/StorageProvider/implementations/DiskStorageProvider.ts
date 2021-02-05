58f912526736f48c67f3e001d7cf9d015859b1d9
Nathaly Rocha <nathalyfroliveira@gmail.com>
2021-02-05 16:46:26 -0300

    Adiciona moficação nos imports

--- src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/implementations/DiskStorageProvider.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
+import UploadConfig from 'config/upload';
 import IStorageProvider from '../models/IStorageProvider';
-import UploadConfig from '../../../../../config/upload';
 
 class DiskStorageProvider implements IStorageProvider {
   public async saveFile(file: string): Promise<string> {
